6c14aa8500b96244bae92b64b9c832b52dbe740b
Wener Rodrigo <wener19.wr@gmail.com>
2023-02-13 22:20:24 -0300

    criando validação do botão iniciar

--- quiz/src/components/Welcome.jsx
+++ quiz/src/components/Welcome.jsx
@@ -1,16 +1,23 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
 import Quiz from "../img/quiz.svg";
 
 import "./Welcome.css";
 
 const Welcome = () => {
+  const [quizState, dispacth] = useContext(QuizContext);
+
   return (
     <div id="welcome">
-        <h2>Seja Bem-vindo</h2>
-        <p>Clique no botão abaixo para começar:</p>
-        <button>Iniciar</button>
-        <img src={Quiz} alt="Inicio de Quiz" />
+      <h2>Seja Bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button onClick={() => dispacth({ type: "CHANGE_STATE" })}>
+        Iniciar
+      </button>
+      <img src={Quiz} alt="Inicio de Quiz" />
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
